e682a95c2f97091323b7b9d89b76efced17b9300
garyatwalbirkbeck <56833408+garyatwalbirkbeck@users.noreply.github.com>
2024-07-18 10:29:26 +0000

    Math operators

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ print("Hello"[0])
 num_char = len(input("What is your name?"))
 
 print("Your name has " + str(num_char) + " characters.")
+
+3 + 5
+
+5 - 3
+
+3 * 5
+
+4 / 12
+
+2**4
+print(3 * (3 + 3) / 3 - 3)
